30ebcfccb5c423cdb75b50142871654c11967cd2
Waqas Ahmed Khan <vikikhan88@gmail.com>
2023-11-28 17:14:56 +1100

    comments table migration created

--- database/migrations/2023_11_28_034043_create_comments_table.php
+++ database/migrations/2023_11_28_034043_create_comments_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('comments', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->longText('comments');
+            $table->integer('user_id');
+            $table->integer('reply_to')->nullable()->default(0);
             $table->timestamps();
         });
     }
